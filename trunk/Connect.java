abb810e7123c5e9d41778452ab9c8291ff5a1257
jlowry <jlowry@f8643c59-ab37-0410-9dcc-fddb5469ff1e>
2007-09-28 18:22:55 +0000

    Added license text to files

--- trunk/Connect.java
+++ trunk/Connect.java
@@ -1,4 +1,18 @@
-/**
+/*
+ * Copyright (C) 2007 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  * Name:
  *
  *   Connect.java
@@ -10,7 +24,7 @@
  *   This class can be configured to use the same database drivers as the
  *   appliance so that you can test connections to your database before
  *   configuring a database crawl from the appliance. You must use the same
- *   database drivers that the appliance uses. See the documentation on 
+ *   database drivers that the appliance uses. See the documentation on
  *   http://support.google.com/enterprise/ for details of the specific
  *   JDBC driver versions supported.
  *
