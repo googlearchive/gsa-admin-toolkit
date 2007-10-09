85f6cb383aa9fac2bf903c875defc366746fc0de
jlowry <jlowry@gmail.com>
2007-10-09 18:33:22 +0000

    Improved documentation. Fix problem in which Admin Console doesn't send correct cookie when setting up cookie rule.

--- sso.py
+++ sso.py
@@ -15,18 +15,24 @@
 # limitations under the License.
 
 
-""" Simple web server for testing Cookie Crawl.
+""" Simple web server for testing Cookie Sites.
+
+This is a simple web server for testing the Cookie Sites in a Google
+Search Appliance. Configure the Admin Console as follows:
 
 Crawl and Index
   Crawl URLs:
-
+    Start crawling from: http://www.mycompany.com:8080/
+    Follow patterns: http://www.mycompany.com:8080/
   Cookie Sites:
-    URL of the login page: http://wimbledon.corp.google.com:8080/secure
-    URL pattern for this rule: http://wimbledon.corp.google.com:8080/
+    URL of the login page: http://www.mycompany.com:8080/secure
+    URL pattern for this rule: http://www.mycompany.com:8080/
     Click "Create a New Cookie Rule"
     Enter username "crawler" and password "crawler". Submit form.
     Click "Save Cookie Rule and Close Window".
 
+You can use this program to mimic an Oblix server by running with the
+--test_cookie_path option.
 
 """
 
@@ -70,7 +76,7 @@ class Sso(object):
                 "<form action=login method=POST>"
                 "<input type=hidden name=path value=\"%s\">"
                 "Username: <input type=text name=login><br>"
-                "Password: <input type=psssword name=password><br>"
+                "Password: <input type=password name=password><br>"
                 "<input type=submit>"
                 "</form>") % (path)
 
@@ -78,14 +84,16 @@ class Sso(object):
     return cherrypy.request.headers["host"]
 
   def login(self, login, password, path, msg=None):
+    print cherrypy.request.headers
     if self.test_cookie_path:
       if msg != None:
         return "You got message: %s" % (msg)
       try:
         form_cookie = cherrypy.request.cookie[self.FORM_COOKIE].value
-        sso_cookie = cherrypy.request.cookie[self.SSO_COOKIE].value
-        if sso_cookie != "1":
-          return "Wrong value for %s cookie." % (self.SSO_COOKIE)
+        # Firefox sends sso_cookie but Admin Console in4.6.4.G.70 does not
+        # sso_cookie = cherrypy.request.cookie[self.SSO_COOKIE].value
+        # if sso_cookie != "1":
+        #   return "Wrong value for %s cookie." % (self.SSO_COOKIE)
       except:
         return "You did not send a required cookie."
     if login != None and login.strip() != "":
